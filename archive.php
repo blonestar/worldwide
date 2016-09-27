24910a0422d75ee3cc6ef588fe9e909a2b114b3d
Bojan <bokica@gmail.com>
2016-09-28 00:28:04 +0200

    resources - fixes

--- archive.php
+++ archive.php
@@ -1,4 +1,5 @@
 <?php get_header() ?>
+<?php the_post() ?>
 
 <?php
 	$main_col_width = 10;
@@ -10,28 +11,28 @@
 	
 	$terms = get_terms( array(
 		'taxonomy' => 'resources_tax',
-		'hide_empty' => true,
+		'hide_empty' => false,
 	) );
+
 ?>
 
 <div class="container-wrapper standard-container-wrapper ">
 	<div class="container">
 		<div class="tabs">
 			<div class="row">
-				<a class="tab " href="<?php echo site_url('resources/resource-library') ?>">All</a>
-				<?php 
+				<a class="tab selected" href="<?php echo site_url('resources/resource-library/all') ?>">All</a>
+				<?php
 					$i = 0;
-					foreach ($terms as $term) { $active = ($term->term_id == get_queried_object()->term_id) ? ' selected' : '';
+					foreach ($terms as $term) {
 						if (++$i==5) echo "<br>";
 				?>
-				<a class="tab<?php echo $active ?>" href="<?php echo site_url('resources/resource-library/' . $term->slug) ?>"><?php echo $term->name ?></a>
+				<a class="tab" href="<?php echo site_url('resources/resource-library/' . $term->slug) ?>"><?php echo $term->name ?></a>
 				<?php } ?>
 			</div>
 		</div>
-		<?php wp_reset_query(); ?>
 		<?php if ( have_posts() ) : ?>
 		<div class="library-list">
-			<h2 class="alt-heading"><?php echo  get_queried_object()->name ?></h2>
+			<h2 class="alt-heading"><?php echo get_query_var( 'term' ) ?></h2>
 			<div class="row">
 				<div>
 					<div class="col-sm-12">
@@ -54,5 +55,10 @@
 		<?php endif; ?>
 	</div>
 </div>
+			
+	
+<?php if( have_rows('template_blocks') ): ?>
+	<?php get_template_blocks(get_the_ID()) ?>
+<?php endif; ?>
 
 <?php get_footer() ?> 
